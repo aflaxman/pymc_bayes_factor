915d02964e673b92d0865fb8751cdf5860dac1bb
Abraham Flaxman <abie@alum.mit.edu>
2009-08-26 23:27:53 -0700

    Use MC instead of MCMC
    This correctly calculates Pr[data | M], instead of the strange quantity I was previously calculating

--- model_selection.py
+++ model_selection.py
@@ -34,7 +34,7 @@ import data
 import models
 
 
-def bayes_factor(m1, m2, iter=1e6, burn=25000, thin=1, verbose=0):
+def bayes_factor(m1, m2, iter=1000000):
     """ Approximate the Bayes factor::
         K = Pr[data | m2] / Pr[data | m1]
 
@@ -57,26 +57,57 @@ def bayes_factor(m1, m2, iter=1e6, burn=25000, thin=1, verbose=0):
       of the probability of the data under the current model
       parameters
     iter: int, optional
-      number of iterations of MCMC
-    burn: int, optional
-      number of initial MCMC iters to discard
-    thin: int, optional
-      number of MCMC iters to discard per sample
-    verbose : int, optional
-      amount of output to request from PyMC
+      number of iterations of MC sampling
 
     Results
     -------
     K : estimate of the bayes factor
     """
 
-    mc1 = MCMC(m1)
-    mc1.sample(iter, burn, thin, verbose=verbose)
+    m = Model(m1)
+    data1_logp = []
+    for ii in range(iter):
+        m.draw_from_prior()
+        data1_logp = m1['data_logp'].value
     
-    mc2 = MCMC(m2)
-    mc2.sample(iter, burn, thin, verbose=verbose)
+    m = Model(m2)
+    data2_logp = []
+    for ii in range(iter):
+        m.draw_from_prior()
+        data2_logp = m2['data_logp'].value
 
-    mu_logp = m1['data_logp'].stats()['mean']
-    K = mean(exp(m2['data_logp'].trace() - mu_logp)) / mean(exp(m1['data_logp'].trace() - mu_logp))
+    mu_logp = mean(data1_logp)
+    K = mean(exp(data2_logp - mu_logp)) / mean(exp(data1_logp - mu_logp))
 
     return K
+
+def wikipedia_example(iter=1000):
+    """ Based on the Wikipedia example, 115 heads from 200 coin
+    tosses, m1 = fair coin, m2 = coin with probability q, uniform
+    [0,1] prior on q.
+
+    >>> assert wikipedia_example() > 1.196
+    >>> assert wikipedia_example() < 1.198
+    """
+
+    @deterministic
+    def data_logp():
+         return binomial_like(115, 200, .5)
+    @potential
+    def data_potential(data_logp=data_logp):
+        return data_logp
+    m1 = dict(data_logp=data_logp, data_potential=data_potential)
+
+
+    q = Uniform('q', 0, 1)
+    @deterministic
+    def data_logp(q=q):
+        return binomial_like(115, 200, q)
+
+    @potential
+    def data_potential(data_logp=data_logp):
+        return data_logp
+
+    m2 = dict(q=q, data_logp=data_logp, data_potential=data_potential)
+
+    return bayes_factor(m1, m2, iter=iter)
