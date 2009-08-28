d2b57c85805074cb9ec5862e3a981b912bd623d7
unknown <abie@.clients.nebula2.washington.edu>
2009-08-27 17:24:01 -0700

    make objects instead of dictionaries for models
    more OOPsy

--- data.py
+++ data.py
@@ -12,10 +12,12 @@ plot(data) - generate summary plot of the data
 
 import csv
 from numpy import nan, isnan, array
+from pylab import plot, axis, legend, text, xlabel, ylabel
 
 
-def plot_1(data=all):
-    from pylab import plot, axis, legend
+def plot_1(data=None):
+    if data == None:
+        data = all
     
     hdi75 = [float(d['HDI.1975'] or -1) for d in data]
     hdi05 = [float(d['HDI.2005'] or -1) for d in data]
@@ -30,8 +32,10 @@ def plot_1(data=all):
     ylabel('Total fertility rate')
     
     
-def plot_2(data=all):
-    from pylab import plot, axis, legend, xlabel, ylabel
+def plot_2(data=None):
+    if data == None:
+        data = all
+    
     hdi75 = [float(d['HDI.1975'] or -1) for d in data]
     hdi05 = [float(d['HDI.2005'] or -1) for d in data]
     tfr75 = [float(d['TFR.1975'] or -1) for d in data]
@@ -40,17 +44,32 @@ def plot_2(data=all):
     plot(hdi75, tfr75, 'b.', alpha=1., label='1975')
     plot(hdi05, tfr05, 'r.', alpha=1., label='2005')
     
-    hdi = [[float(d['HDI.%d'%y] or -1) for y in range(1976,2005)] for d in data]
-    tfr = [[float(d['TFR.%d'%y] or -1) for y in range(1976,2005)] for d in data]
-
-    for x, y in zip(hdi, tfr):
-        plot(x, y, 'k.', alpha=.3)
+    plot(hdi, tfr, 'k.', alpha=.3, zorder=0.)
 
     axis([.3, 1, 1, 8])
     legend()
     xlabel('Human development index')
     ylabel('Total fertility rate')
 
+def plot_3(data=None):
+    if data == None:
+        data = all
+
+    plot_2(data)
+    
+    hdi = [[float(d['HDI.%d'%y] or 0) for y in range(1975,2006)] for d in data]
+    tfr = [[float(d['TFR.%d'%y] or 0) for y in range(1975,2006)] for d in data]
+    country = [d['country'] for d in data]
+
+    for x, y, c in zip(hdi, tfr, country):
+        if max(x) < .86:
+            continue
+        
+        plot(x, y, 'k', alpha=.1,zorder=-1.)
+        #text(x[-1], y[-1], '  ' + c, fontsize=8,
+        #     color='r', alpha=.75, verticalalignment='center')
+
+    axis([.86, .97, 1, 3.1])
 
 def to_arrays(data):
     """ Convert list of data dicts to array
@@ -66,7 +85,7 @@ def to_arrays(data):
     hdi = []
     tfr = []
     for d in data:
-        for y in range(1976, 2006):
+        for y in range(1975, 2006):
             if d['HDI.%d'%y] == '' or d['TFR.%d'%y] == '':
                 continue
             hdi.append(float(d['HDI.%d'%y]))
--- models.py
+++ models.py
@@ -10,6 +10,14 @@ piecewise_linear(X, Y, breakpoint=.86)
          { beta[0] + beta[1] * (X[i]-breakpoint) + N(0, sigma^2) if X[i] < breakpoint
   Y[i] ~ {
          { beta[0] + beta[2] * (X[i]-breakpoint) + N(0, sigma^2) if X[i] >= breakpoint
+
+Examples
+--------
+>>> from pymc import *
+>>> import data
+>>> m1 = models.linear(X=data.hdi, Y=data.tfr, order=2, mu_beta=[0., -8, 8], sigma_beta=5., mu_sigma=.01)
+>>> MCMC(m1).sample(25000, 10000, 15)
+>>>
 """
 
 
@@ -17,71 +25,90 @@ from pymc import *
 from numpy import zeros, ones, array, polyval
 
 
-def linear(X, Y, order=1):
-    """ Make a Bayesian model for::
-    
-        Y[i] ~ beta[0] * X[i]^order + ... + beta[order-1] * X[i] + beta[order] + N(0, sigma^2),
+class linear:
+    def __init__(self, X, Y, order=1, mu_beta=None, sigma_beta=1., mu_sigma=1.):
+        """ Make a Bayesian model for::
+
+            Y[i] ~ beta[0] * X[i]^order + ... + beta[order-1] * X[i] + beta[order] + N(0, sigma^2),
+
+        with priors::
+
+            beta[i] ~ N(mu=mu_beta, tau=1/sigma_beta**2)
+            sigma ~ Gamma(alpha=1, beta=1/mu_sigma)
+
+        Parameters
+        ----------
+        X : list of floats
+        Y : list of floats
+        mu_beta : list of floats, optional
+        sigma_beta : float or list of floats, optional
+        mu_sigma : float, optional
+        order : int, optional
+          the order of the fit polynomial
+
+        Results
+        -------
+        m : an object containing all relevant PyMC stochastic
+        variables and also a prediction function
+        """
+        if mu_beta == None:
+            mu_beta = zeros(order+1)
+
+        self.beta = Normal('beta', mu_beta, sigma_beta**-2)
+        self.sigma = Gamma('standard error', 1., 1./mu_sigma)
+
+        @potential
+        def data_potential(beta=self.beta, sigma=self.sigma,
+                           X=X, Y=Y):
+            mu = self.predict(beta, X)
+            return normal_like(Y, mu, 1 / sigma**2)
+        self.data_potential = data_potential
+
+    def predict(self, beta, X):
+        return polyval(beta, X)
+
+
+class piecewise_linear:
+    def __init__(self, X, Y, breakpoint=.86,
+                 mu_beta=[0., 0., 0.], sigma_beta=1., mu_sigma=1.):
+        """ Make a Bayesian model for::
+                 { beta[0] + beta[1] * (X[i]-breakpoint) + N(0, sigma^2) if X[i] < breakpoint
+          Y[i] ~ {
+                 { beta[0] + beta[2] * (X[i]-breakpoint) + N(0, sigma^2) if X[i] >= breakpoint
+
+        with priors::
+
+            beta[i] ~ N(mu=mu_beta, tau=1/sigma_beta**2)
+            sigma ~ Gamma(alpha=1, beta=1/mu_sigma)
+
+        Parameters
+        ----------
+        X : list of floats
+        Y : list of floats
+        breakpoint : float or PyMC stoch
+          the x value to switch from one linear model to the other
+        mu_beta : list of floats, optional
+        sigma_beta : float or list of floats, optional
+        mu_sigma : float, optional
+
+        Results
+        -------
+        m : an object containing all relevant PyMC stochastic
+        variables and also a prediction function
+        """
+        self.breakpoint=breakpoint
+        self.beta = Normal('beta', mu_beta, sigma_beta**-2)
+        self.sigma = Gamma('standard error', 1., 1./mu_sigma)
+
+        @potential
+        def data_potential(beta=self.beta, sigma=self.sigma,
+                           X=X, Y=Y, breakpoint=self.breakpoint):
+            mu = self.predict(beta, breakpoint, X)
+            return normal_like(Y, mu, 1 / sigma**2)
+        self.data_potential = data_potential
         
-    with priors::
-
-        beta[i] ~ N(mu=0, tau=1)
-        sigma ~ Gamma(alpha=1, beta=1)
-
-    Parameters
-    ----------
-    X : list of floats
-    Y : list of floats
-    order : int, optional
-      the order of the fit polynomial
-
-    Results
-    -------
-    m : dict of PyMC variables, including a deterministic called 'data_logp'
-    """
-    beta = Normal('beta', zeros(order+1), 1.)
-    sigma = Gamma('standard error', 1., 1.)
-
-    @potential
-    def data_potential(beta=beta, sigma=sigma, X=X, Y=Y):
-        return normal_like(Y, polyval(beta, X), 1. / sigma**2)
-
-    return dict(X=X, Y=Y,
-                beta=beta, sigma=sigma,
-                data_potential=data_potential)
-
-    
-def piecewise_linear(X, Y, breakpoint=.86):
-    """ Make a Bayesian model for::
-             { beta[0] + beta[1] * (X[i]-breakpoint) + N(0, sigma^2) if X[i] < breakpoint
-      Y[i] ~ {
-             { beta[0] + beta[2] * (X[i]-breakpoint) + N(0, sigma^2) if X[i] >= breakpoint
-
-    with priors::
-
-        beta[i] ~ N(mu=0, tau=1)
-        sigma ~ Gamma(alpha=1, beta=1)
-
-    Parameters
-    ----------
-    X : list of floats
-    Y : list of floats
-    breakpoint : float or PyMC stoch
-      the x value to switch from one linear model to the other
-
-    Results
-    -------
-    m : dict of PyMC variables
-    """
-    beta = Normal('beta', [0., 0., 0.], .1)
-    sigma = Gamma('standard error', 1., 1.)
-
-    @potential
-    def data_potential(beta=beta, sigma=sigma, X=X, Y=Y, breakpoint=breakpoint):
-        very_high_dev_indicator = X >= breakpoint
-        mu = (beta[0] + beta[1]*(X-breakpoint)) * (1 - very_high_dev_indicator)
-        mu += (beta[0] + beta[2]*(X-breakpoint)) * very_high_dev_indicator
-        return normal_like(Y, mu, 1 / sigma**2)
-
-    return dict(X=X, Y=Y, breakpoint=breakpoint,
-                beta=beta, sigma=sigma,
-                data_potential=data_potential)
+    def predict(self, beta, breakpoint, X):
+            very_high_dev_indicator = X >= breakpoint
+            mu = (beta[0] + beta[1]*(X-breakpoint)) * (1 - very_high_dev_indicator)
+            mu += (beta[0] + beta[2]*(X-breakpoint)) * very_high_dev_indicator
+            return mu
